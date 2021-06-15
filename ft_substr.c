81255328de7a2e108ed1ee43fe37d5d91a9bb92c
Alexandra Gallipoli <83506452+agallipo@users.noreply.github.com>
2021-06-15 12:33:11 +0200

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -2,14 +2,23 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*ptr;
+	char		*ptr;
+	unsigned int	i;
 
-	ptr = malloc((len + 1) * sizeof(char));
-	if (!ptr || !s)
+	i = 0;
+	if (!s)
+		return (NULL);
+//	ptr = (char *)malloc((len + 1) * sizeof(char));
+	if (!ptr)
 		return (NULL);
 	if (start > ft_strlen(s) - 1)
 		return (ft_strdup(""));
-	ft_memcpy(ptr, &s[start], len);
-	ptr[len] = '\0';
+	ptr = (char *)malloc((len + 1) * sizeof(char));
+	while (len > 0 || s[start++] != '\0')
+	{
+		ptr[i++] = s[start++];
+		len--;
+	}
+	ptr[i] = '\0';
 	return (ptr);
 }
